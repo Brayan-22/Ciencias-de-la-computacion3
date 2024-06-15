dc6aeaab7929cbc08a541155c0b8198c0b04a1e9
alejandro <alejandroriveros100@gmail.com>
2024-06-15 00:37:43 -0500

    [remove] Unnecesary files

--- compilador/src/resources/DoWhile.java
+++
@@ -1,8 +0,0 @@
-public class DoWhile {
-    public static void main(String[] args) {
-        int i = 0;
-        do {
-            i++;
-        } while (i<10);
-    }
-}
--- compilador/src/resources/While.java
+++
@@ -1,8 +0,0 @@
-public class While {
-    public static void main(String[] args) {
-        int i = 0;
-        while (i<10) {
-            i++;
-        }
-    }
-}
